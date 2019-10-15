0d1a129d20ec5109534c0349c0c59c129ef0a427
Vyacheslav Kazarinov <vyacheslav.kazarinov@incode-it.com>
2019-10-15 17:42:47 +0300

    add location service and test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,8 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+import * as request from 'supertest';
+
+describe('LocationController (e2e)', () => {
   let app;
 
   beforeEach(async () => {
