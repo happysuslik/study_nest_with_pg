5ac4ead221a0d1279e0258474dbbeca2dfc0c955
Vyacheslav Kazarinov <vyacheslav.kazarinov@incode-it.com>
2019-10-15 16:39:25 +0300

    add hbs views and fix e2e tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,14 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return locations', () => {
+      expect(appController.listLocations()).toMatchObject({
+        locations: [
+          'Location 1',
+          'Location 2',
+          'Location 3',
+        ],
+      });
     });
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { resolve } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const APP_DIRECTORY = resolve(__dirname, '..');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(resolve(APP_DIRECTORY, 'public'));
+  app.setBaseViewsDir(resolve(APP_DIRECTORY, 'views'));
+  app.setViewEngine('hbs');
+
   await app.listen(3000);
 }
 bootstrap();
