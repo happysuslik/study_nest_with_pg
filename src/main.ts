3aa070d83bf4c9a4cf84219efc712a4f64bc579e
Vyacheslav Kazarinov <vyacheslav.kazarinov@incode-it.com>
2019-10-16 10:28:36 +0300

    feature: providing a config

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { resolve } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
-  const APP_DIRECTORY = resolve(__dirname, '..');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const config = app.get(ConfigService);
+  const viewsDirectory = config.get<string>('templates.path');
+  const publicDirectory = config.get<string>('public.path');
 
-  app.useStaticAssets(resolve(APP_DIRECTORY, 'public'));
-  app.setBaseViewsDir(resolve(APP_DIRECTORY, 'views'));
   app.setViewEngine('hbs');
+  app.useStaticAssets(publicDirectory);
+  app.setBaseViewsDir(viewsDirectory);
 
   await app.listen(3000);
 }
