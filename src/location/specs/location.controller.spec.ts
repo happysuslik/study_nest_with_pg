a93327b3d5922fdc209ee68812a7c9c0e4045e1e
Vyacheslav Kazarinov <vyacheslav.kazarinov@incode-it.com>
2019-10-18 17:02:59 +0300

    tests: fix location controller

--- src/location/specs/location.controller.spec.ts
+++ src/location/specs/location.controller.spec.ts
@@ -1,23 +1,52 @@
-// import { LocationController } from '../location.controller';
-// import { LocationService } from '../services/location.service';
-//
-// describe('AppController', () => {
-//   let locationController: LocationController;
-//   let locationService: LocationService;
-//
-//   beforeEach(async () => {
-//     locationService = new LocationService();
-//     locationController = new LocationController(locationService);
-//   });
-//
-//   describe('root', () => {
-//     it('should return locations', () => {
-//       expect(locationController.listLocations()).toMatchObject({
-//         locations: [
-//           'Location 1',
-//           'Location 2',
-//         ],
-//       });
-//     });
-//   });
-// });
+import {LocationController} from '../location.controller';
+import {LocationService} from '../services/location.service';
+import {MappingRegistryService} from '../../common/mapping-registry.service';
+import {MockHelper} from '../../common/mock.helper';
+import {GenericFactory} from '../../common/generic.factory';
+import {Location} from '../entities/location.entity';
+import {LocationMappingService} from '../services/location-mapping.service';
+
+describe('Location Controller', () => {
+    let locationController: LocationController;
+    let mockLocationService: LocationService;
+    let mappingRegistryService: MappingRegistryService;
+
+    beforeEach(async () => {
+        mockLocationService = MockHelper.mock<LocationService>({
+            list: {
+                resolves: [
+                    GenericFactory.create<Location>(Location, {
+                        id: 'e0537985-9f9b-44cb-afe8-687f4be39147',
+                        name: 'Location 1',
+                    }),
+                    GenericFactory.create<Location>(Location, {
+                        id: 'abd4d5a1-fef3-436a-b501-c46363b4cba7',
+                        name: 'Location 2',
+                    }),
+                ],
+            },
+        });
+
+        mappingRegistryService = new MappingRegistryService();
+        const locationMappingService = new LocationMappingService(mappingRegistryService);
+
+        locationController = new LocationController(mockLocationService, mappingRegistryService);
+    });
+
+    describe('root', () => {
+        it('should return locations', () => {
+            expect(locationController.listLocations()).resolves.toMatchObject({
+                locations: [
+                    {
+                        id: 'e0537985-9f9b-44cb-afe8-687f4be39147',
+                        name: 'Location 1',
+                    },
+                    {
+                        id: 'abd4d5a1-fef3-436a-b501-c46363b4cba7',
+                        name: 'Location 2',
+                    },
+                ],
+            });
+        });
+    });
+});
